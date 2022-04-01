70e1696f7318213b7ec979dcb64b275baf12d23f
abhishek <abhishekshinde61@gmail.com>
2022-04-01 12:48:55 +0530

    second commit

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const postData = require('./mock/posts.json')
 
 
+app.get('/', (req, res) => {
+    res.send("hello")
+})
+
+app.get('/posts', (req, res) => {
+    res.send(postData.posts)
+})
 
 
 
